97482c7ab68f51e64f79d14d474ca04be0a83d24
Phi Hai <dangphihai@gmail.com>
2020-02-28 15:16:02 +0700

    second commit

--- MyCalculator/CalculatorLogic.swift
+++ MyCalculator/CalculatorLogic.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+struct CalculatorLogic {
+    
+    private var number: Double?
+    
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double) {
+        self.number = number
+    }
+    
+    mutating func calculate(symbol: String) -> Double? {
+        if let n = number {
+            switch symbol {
+            case "AC", "C":
+                return 0
+            case "+/-":
+                return n * -1
+            case "%":
+                return n * 0.01
+            case "=":
+                return performTwoNumCal (n2: n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        return nil
+    }
+    
+    private func performTwoNumCal(n2: Double) -> Double? {
+        
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            default:
+                return n1 / n2
+            }
+        }
+        return nil
+    }
+    
+}
