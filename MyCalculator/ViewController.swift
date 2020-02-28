4f98c301c586788c0745d2e9ef03e625beed9c31
Phi Hai <dangphihai@gmail.com>
2020-02-28 13:51:05 +0700

    Initial Commit

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Phi Hải on 2/28/20.
+//  Copyright © 2020 Phi Hải. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
